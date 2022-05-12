4400e04640e3c6070df97d878fc1916473d5abc6
Shoozle <sean@seanmurtagh.uk>
2022-05-12 18:16:36 +0100

    test data for pinata, pinatas page

---
+++ src/pages/Pinatas.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+import styled from "styled-components"
+
+function Pinatas() {
+
+   return (
+      <div>
+      </div>
+   )
+}
+
+const Cards = styled.div`
+   
+`
+
+export default Pinatas
+
