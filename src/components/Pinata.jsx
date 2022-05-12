2fe2d624d44c7af722dded3fa31c26f389a7ef12
Shoozle <sean@seanmurtagh.uk>
2022-05-12 18:39:13 +0100

    click to hide/show li items

--- src/components/Pinata.jsx
+++ src/components/Pinata.jsx
@@ -10,53 +10,53 @@ function Pinata() {
    const [trick, setTrick] = useState(false);
    const [variants, setVariants] = useState(false);
    const [wildcard, setWildcard] = useState(false);
-   const [users, setUses] = useState(true);
+   const [uses, setUses] = useState(true);
 
 
    return (
       <Card>
          <h3>Fudgehog</h3>
-         <h4>Appear Requirements</h4>
+         <h4 onClick={() => {setAppear(!appear)}} >Appear Requirements</h4>
          <ul className={appear ? 'visible' : 'invisible'}>
             <li>Have 1 Holly bush in the garden</li>
          </ul>
-         <h4>Visit Requirements</h4>
+         <h4 onClick={() => {setVisit(!visit)}}>Visit Requirements</h4>
          <ul className={visit ? 'visible' : 'invisible'}>
             <li>Have 1 Holly bush in the garden</li>
             <li>Have 1 Thistle in the garden</li>
          </ul>
-         <h4>Resident Requirements</h4>
+         <h4 onClick={() => {setResident(!resident)}}>Resident Requirements</h4>
          <ul className={resident ? 'visible' : 'invisible'}>
             <li>Has eaten 1 Holly berry</li>
             <li>Has eaten 1 Thistle</li>
             <li>Have 1 Leaf Pile in the garden</li>
          </ul>
-         <h4>Romance Requirements</h4>
+         <h4 onClick={() => {setRomance(!romance)}}>Romance Requirements</h4>
          <ul className={romance ? 'visible' : 'invisible'}>
             <li>Has drunk a bottle of milk</li>
             <li>Has eaten a loaf of bread</li>
             <li>There is a Fudgehog house in the garden</li>
          </ul>
-         <h4>Trick Requirements</h4>
+         <h4 onClick={() => {setTrick(!trick)}}>Trick Requirements</h4>
          <ul className={trick ? 'visible' : 'invisible'}>
             <li>Feed it a Garlic seed to have it perform Trick 1</li>
             <li>Feed it a Bulb of Garlic to have it perform Trick 2</li>
          </ul>
-         <h4>Species + Color Variants</h4>
+         <h4 onClick={() => {setVariants(!variants)}}>Species + Color Variants</h4>
          <ul className={variants ? 'visible' : 'invisible'}>
             <li>Feeding a coconut to a Fudgehog turns it into a Parmadillo </li>
             <li>Feeding it a bluebell seed changes its color to blue</li>
             <li>Feeding it poison ivy changes its color to green</li>
             <li>Feeding it a chili changes its color to red</li>
          </ul>
-         <h4>Wildcard Versions</h4>
+         <h4 onClick={() => {setWildcard(!wildcard)}}>Wildcard Versions</h4>
          <ul className={wildcard ? 'visible' : 'invisible'}>
             <li>Bumps</li>
             <li>Ears</li>
             <li>Three Horns</li>
          </ul>
-         <h4>Fudgehog Uses</h4>
-         <ul className={appear ? 'visible' : 'invisible'}>
+         <h4 onClick={() => {setUses(!uses)}}>Fudgehog Uses</h4>
+         <ul className={uses ? 'visible' : 'invisible'}>
             <li>Feed a Fudgehog to a Badgesicle to teach it trick 1</li>
          </ul>
       </Card>
